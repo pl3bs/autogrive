2a67c6dbdef92e76dbdc20e8972146460a4b68ed
pl3bs <brandonchapman@gmx.com>
2016-01-29 18:31:07 -0600

    Create backup.sh

---
+++ backup.sh
@@ -0,0 +1,18 @@
+cd /
+date=$(date -I)
+
+#create backup of system
+
+tar -cvpjf "$date".tar.bz2 --exclude=/"$date".tar.bz2 --one-file-system /
+
+# move backup and sync to drive
+
+mv "$date".tar.bz2 /root/backup
+
+cd /root/backup
+
+grive sync
+
+# remove local backup post-sync
+
+rm "$date"*
