edc4378c48932bb45e4db3d4fd8453176b5bc2e8
pl3bs <brandonchapman@gmx.com>
2016-01-29 18:29:40 -0600

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,6 +1,11 @@
+echo "2 2 * * 1       root    cd ~/ && ./backup.sh" >> /etc/crontab
 yes | add-apt-repository ppa:nilarimogard/webupd8
 apt-get update -y
-apt-get install grive
+apt-get install grive -y
 mkdir /root/backup
 cd /root/backup
 grive -a
+
+wget blah
+./blah
+
